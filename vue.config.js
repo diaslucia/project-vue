b9da12e6f893887a4d7ec415d1924bb82f357a2f
diaslucia <luciadias.dev@gmail.com>
2023-08-01 19:38:33 -0300

    add github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.VUE_APP_BASE_URL,
+  publicPath:
+    process.env.VUE_APP_BASE_URL === "production" ? "/project-vue/" : "/",
 });
